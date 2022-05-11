e833c01ec15b88235c59e00f0f69bc78b7395ae9
Angelo Danrley <82960620+maikaodev@users.noreply.github.com>
2022-05-11 17:08:32 -0300

    Removing the property to instantiate on variable

--- src/models/Playlist.ts
+++ src/models/Playlist.ts
@@ -1,10 +1,9 @@
 import { Album } from './Album';
 export class Playlist implements PlaylistType {
   albums: AlbumType[] = [];
-  Album: Album | undefined;
   addAlbum(data: AlbumData) {
-    this.Album = new Album(data);
-    this.albums.push(this.Album);
+    const _Album = new Album(data);
+    this.albums.push(_Album);
   }
   isFirstAlbum(index: number): boolean {
     if (index === 0) {
