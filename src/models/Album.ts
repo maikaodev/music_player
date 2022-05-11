915ba0536f4c8df2ed4d4881742dccfb4a1bb758
Angelo Danrley <82960620+maikaodev@users.noreply.github.com>
2022-05-11 13:49:55 -0300

    Finished album test

--- src/models/Album.ts
+++ src/models/Album.ts
@@ -1,3 +1,33 @@
 export class Album implements AlbumType {
-  // Code here...
+  constructor(data: AlbumData) {
+    this.artist = data.artist;
+    this.cover = data.cover;
+    this.title = data.title;
+    this.tracks = data.tracks;
+  }
+  artist: string;
+  cover: string;
+  title: string;
+  tracks: TrackData[];
+  getUrlFromIndex(index: number): string | null {
+    if (index === 0 && index < this.tracks.length - 1) {
+      return this.tracks[index].url;
+    } else {
+      return null;
+    }
+  }
+  isFirstTrack(index: number): boolean {
+    if (index === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  isLastTrack(index: number): boolean {
+    if (index === this.tracks.length - 1) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
