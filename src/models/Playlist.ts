f2e74c470099d2c374901795755c9b57ff84ff12
Angelo Danrley <82960620+maikaodev@users.noreply.github.com>
2022-05-11 16:46:41 -0300

    Finished playlist test

--- src/models/Playlist.ts
+++ src/models/Playlist.ts
@@ -1,3 +1,23 @@
+import { Album } from './Album';
 export class Playlist implements PlaylistType {
-  // Code here...
+  albums: AlbumType[] = [];
+  Album: Album | undefined;
+  addAlbum(data: AlbumData) {
+    this.Album = new Album(data);
+    this.albums.push(this.Album);
+  }
+  isFirstAlbum(index: number): boolean {
+    if (index === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  isLastAlbum(index: number): boolean {
+    if (index === this.albums.length - 1) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
