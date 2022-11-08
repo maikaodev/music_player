42fe0d15001de964bf522eae5ea28007a3856b71
maikaodev <maikao.dev@gmail.com>
2022-11-08 20:31:03 -0300

    Refactoring the models

--- src/models/Playlist.ts
+++ src/models/Playlist.ts
@@ -2,23 +2,29 @@ import { Album } from './Album';
 import albums from '~/mocks/albums.json';
 
 export class Playlist implements PlaylistType {
-  albums: AlbumType[] = [];
+  //
+  albums: AlbumType[];
+
+  constructor() {
+    this.albums = [];
+  }
+
   addAlbum(data: AlbumData) {
     const _Album = new Album(data);
     this.albums.push(_Album);
   }
+
   isFirstAlbum(index: number): boolean {
     if (index === 0) {
       return true;
-    } else {
-      return false;
     }
+    return false;
   }
+
   isLastAlbum(index: number): boolean {
     if (index === albums.length - 1) {
       return true;
-    } else {
-      return false;
     }
+    return false;
   }
 }
